4f707ba3c60e9131856f819233f385147f211da3
Trevor Power <trevor_power@yahoo.com>
2011-10-25 13:31:09 +0100

    removed links and replaced with sub directories for aliasing

--- lib/mongo_san/tasks.rb
+++ lib/mongo_san/tasks.rb
@@ -8,13 +8,12 @@ end
 namespace :heroku do
   namespace :mongo do
     desc "Dump the mongo db content"
-    task :dump => :environment do
+    task :dump, [:alias]  => :environment do |t, args|
       each_heroku_app do |name, app|
         puts "#{name}:"
         uri = URI.parse(heroku_env(app)["MONGOHQ_URL"])
         db = uri.path.delete '/'
-        puts `mongodump -h #{uri.host}:#{uri.port} -u #{uri.user} -p #{uri.password} -d #{db}`
-        puts `ln -sf #{db} dump/#{name}`
+        puts `mongodump -h #{uri.host}:#{uri.port} -u #{uri.user} -p #{uri.password} -d #{db} -o dump/#{args[:alias] || name}`
       end
     end
 
@@ -24,7 +23,7 @@ namespace :heroku do
         puts "#{name}:"
         uri = URI.parse(heroku_env(app)["MONGOHQ_URL"])
         db = uri.path.delete '/'
-        puts `mongorestore -h #{uri.host}:#{uri.port} -u #{uri.user} -p #{uri.password} -d #{db} dump/#{args[:alias] || name}`
+        puts `mongorestore -h #{uri.host}:#{uri.port} -u #{uri.user} -p #{uri.password} -d #{db} dump/#{args[:alias] || name}/*`
       end
     end
   end
